8871c03238835fc899150b295e2d9e40f883e44c
Emre Malçok <emremalcok44@gmail.com>
2025-07-24 15:42:15 +0300

    First Commit

---
+++ todo_app/lib/pages/add_new_task.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddNewTaskPage extends StatelessWidget {
+  const AddNewTaskPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("Add New Task"));
+  }
+}
---
+++ todo_app/lib/pages/home.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:todo_app/constans/color.dart';
+import 'package:todo_app/pages/add_new_task.dart';
+import 'package:todo_app/todoitem.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isChecked = false;
+  List<String> todo = [
+    "Go to Work",
+    "Study on Flutter",
+    "Share Instagram Posts",
+  ];
+  List<String> completed = ["Social Media Shares", "Meeting", "Make Designs"];
+
+  @override
+  Widget build(BuildContext context) {
+    double deviceHeight = MediaQuery.of(context).size.height;
+    double deviceWidth = MediaQuery.of(context).size.width;
+    DateTime date = DateTime.now();
+
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: HexColor(backgroundColor),
+          body: Column(
+            children: [
+              // Header
+              Container(
+                width: deviceWidth,
+                height: deviceHeight / 4,
+                color: Colors.purple,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 30.0),
+                    Text(
+                      '$date',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 20)),
+                    Text(
+                      "My Todo List",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // Top Column
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                  child: SingleChildScrollView(
+                    child: ListView.builder(
+                      primary: false,
+                      shrinkWrap: true, // takes space as it contains
+                      itemCount: todo.length,
+                      itemBuilder: (context, index) {
+                        return TodoItem(title: todo[index]);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              // Completed Text
+              const Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Completed",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+              // Bottom Column
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                  child: SingleChildScrollView(
+                    child: ListView.builder(
+                      primary: false, // generally in ListView
+                      shrinkWrap: true, // generally in ListView
+                      itemCount: completed.length,
+                      itemBuilder: (context, index) {
+                        return TodoItem(title: completed[index]);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              // Button at the Bottom
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const AddNewTaskPage(),
+                    ),
+                  );
+                },
+                child: const Text("Add New Task"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
